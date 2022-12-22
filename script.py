dc097e0adcc54e5ab073b5325bf3ec4f97d07c57
joelcoelho_ <joelfeco4@gmail.com>
2022-12-21 23:54:21 -0300

    finished project

--- script.py
+++ script.py
@@ -6,32 +6,42 @@ import math
 from plot import plotTSP
 
 def find_next(X, j):
-	for i in range(n):
-		if round(X[j,i].X) == 1:
-			next = i		
-	return next
+    '''
+    Essa função retorna o próximo ponto focal a ser visitado
+    '''
+    for i in range(n):
+        if round(X[j,i].X) == 1:
+            next = i		
+    return next
 
-def ver_subtour(X,j):
-	subtour = [j]
-	next = find_next(X,j)
-	while next != j:
-		subtour.append(next)
-		next = find_next(X,next)
-	return subtour
+def display_subtour(X,j):
+    '''
+    Essa função retorna a rota a ser percorrida
+    '''
+    subtour = [j]
+    next = find_next(X,j)
+    while next != j:
+        subtour.append(next)
+        next = find_next(X,next)
+    return subtour
 
-def escreve_sol(X):
-	solucao = []
-	for j in range(n):
-		j_sol = 0
-		for i in range(len(solucao)):
-			if j in solucao[i]:
-				j_sol = 1			
-		if j_sol == 0:
-			subtour = ver_subtour(X,j)
-			solucao.append(subtour)
-	return solucao
+def write_solution(X):
+    '''
+    Essa função retorna a solução do problema
+    '''
+    solution = []
+    for j in range(n):
+        j_sol = 0
+        for i in range(len(solution)):
+            if j in solution[i]:
+                j_sol = 1			
+        if j_sol == 0:
+            subtour = display_subtour(X,j)
+            solution.append(subtour)
+    return solution
 
-with open('burma14.tsp', 'r') as f:
+# Le as coordenadas do arquivo e salva na variável data
+with open('coordinates_map.tsp', 'r') as f:
     data = [[num for num in line.split()] for line in f]
 
 n = int(data[3][-1])
@@ -49,77 +59,28 @@ for j in range(n):
         dist = math.sqrt( ((coord[j][0]-coord[i][0])**2)+((coord[j][1]-coord[i][1])**2) )
         d[j].append(dist)
 
-def exemplo1():
-    model = gp.Model("Dantzig")
-    gp.setParam('LogToConsole', 0)
-
-    X = {}
-    X = model.addVars(n,n,vtype=GRB.BINARY)
-
-    distance = sum(X[j,i]*d[j][i] for j in range(n) for i in range(n))
-    model.setObjective(distance, GRB.MINIMIZE)
-
+def addConstraints(model, X, u):
+     # Essa constraint garante que exista uma viagem de chegada para cada ponto focal
     model.addConstrs(sum(X[j,i] for i in range(n)) == 1 for j in range(n))
+
+    # Essa constraint garante que exista uma viagem saindo de cada ponto focal
     model.addConstrs(sum(X[j,i] for j in range(n)) == 1 for i in range(n))
+
+    # Essa constraint garante que não exista uma viagem de um ponto X ao mesmo ponto X
     model.addConstrs(X[j,j] == 0 for j in range(n))
+
+    # Essa constraint garante a sequência de idas a pontos focais descrita no início do documento
+    model.addConstrs(X[i,i+1] == 1 for i in range(4, 11))
+
+    # Essa constraint garante a inexistência de subtours descritos no documento
+    model.addConstrs(u[i] - u[j] <= n*(1-X[i,j]) - 1
+                    for j in range(n) for i in range(1,n) if i != j)
+
     model.update()
 
-    start_time = time.time()
 
-    while True:
-        model.optimize()	
-        solucao = escreve_sol(X)
-        n_rotas = len(solucao)
-        print("\nNúmero de rotas: "+str(n_rotas))
-        print(solucao)
-        print("Valor: "+str(model.objVAL))
-
-        if n_rotas != 1:
-            for r in range(n_rotas):
-                model.addConstr(sum(X[j,i] for j in solucao[r] for i in solucao[r]) <= len(solucao[r]) - 1)
-                model.update()
-            else:
-                break
-
-    end_time = time.time()
-    print("\nTempo total: "+str(round(end_time-start_time,2)))
-
-def exemplo2():
+def solve():
     model = gp.Model("Dantzig")
-    gp.setParam('LogToConsole', 0)
-
-    X = {}
-    X = model.addVars(n,n,vtype=GRB.BINARY)
-
-    distance = sum(X[j,i]*d[j][i] for j in range(n) for i in range(n))
-    model.setObjective(distance, GRB.MINIMIZE)
-
-    model.addConstrs(sum(X[j,i] for i in range(n)) == 1 for j in range(n))
-    model.addConstrs(sum(X[j,i] for j in range(n)) == 1 for i in range(n))
-    model.addConstrs(X[j,j] == 0 for j in range(n))
-    model.update()
-
-    start_time = time.time()
-
-    while True:
-        model.optimize()	
-        solucao = escreve_sol(X)
-        n_rotas = len(solucao)
-        print("\nNúmero de rotas: "+str(n_rotas))
-        print(solucao)
-        print("Valor: "+str(model.objVAL))
-        if n_rotas != 1:
-            for r in range(n_rotas):
-                model.addConstr(sum(X[j,i] for j in solucao[r] for i in solucao[r]) <= len(solucao[r]) - 1)
-                model.update()
-        else:
-            break
-
-    end_time = time.time()
-    print("\nTempo total: "+str(round(end_time-start_time,2)))
-
-def exemplo3():
-    model = gp.Model("MTZ")
     gp.setParam('LogToConsole', 0)
 
     X = {}
@@ -128,27 +89,31 @@ def exemplo3():
     u = model.addVars(n,vtype=GRB.CONTINUOUS,lb=0)
 
     distance = sum(X[j,i]*d[j][i] for j in range(n) for i in range(n))
+
+    # Objetivo é minimizar a distância (distance)
     model.setObjective(distance, GRB.MINIMIZE)
 
-    model.addConstrs(sum(X[j,i] for i in range(n)) == 1 for j in range(n))
-    model.addConstrs(sum(X[j,i] for j in range(n)) == 1 for i in range(n))
-    model.addConstrs(X[j,j] == 0 for j in range(n))
-
-    model.addConstrs(u[i] - u[j] <= n*(1-X[i,j]) - 1 
-                    for j in range(n) for i in range(1,n) if i != j)
-
-    model.update()
+    addConstraints(model, X, u)
 
     start_time = time.time()
-    model.optimize()	
-    solucao = escreve_sol(X)
-    n_rotas = len(solucao)
-    print(solucao)
-    print("Valor: "+str(model.objVAL))
+
+    while True:
+        model.optimize()	
+        solution = write_solution(X)
+        number_of_routes = len(solution)
+        print("\nNúmero de rotas: "+str(number_of_routes))
+        print(solution)
+        print("Valor: "+str(model.objVAL))
+        if number_of_routes != 1:
+            for r in range(number_of_routes):
+                model.addConstr(sum(X[j,i] for j in solution[r] for i in solution[r]) <= len(solution[r]) - 1)
+                model.update()
+        else:
+            break
+
     end_time = time.time()
-    print("\nTempo total: "+str(round(end_time-start_time,2)))
+    print("\nTempo total: "+ str(round(end_time-start_time,2)) + "segundos")
 
-    plotTSP(solucao[0], coord)
+    plotTSP(solution[0], coord)
 
-
-exemplo3()
+solve()
